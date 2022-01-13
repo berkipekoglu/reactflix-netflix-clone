8c7ff852d7b19df8ce034a9e62808792fdf1b98d
berkipekoglu <berkipekoglu@gmail.com>
2022-01-13 17:52:07 +0300

    componentler oluşturuldu, stiller yapıldı.

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,10 +1,17 @@
+import Featured from '../components/featured/Featured';
+import List from '../components/list/List';
+import Navbar from '../components/navbar/Navbar';
 import './Home.scss';
 //import { AcUnit } from '@mui/icons-material'
 
 const Home = () => {
     return (
         <div className="home">
-           
+          <Navbar />
+          <Featured type="movie" />
+          <List />
+          <List />
+          <List />
         </div>
     )
 }
