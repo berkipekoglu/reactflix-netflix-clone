c3382341f479649ab8fb267b3770ae3c4a33c8be
berkipekoglu <berkipekoglu@gmail.com>
2022-01-14 17:41:33 +0300

    kayıt, giriş ve izleme sayfaları oluşturuldu, tasarlandı.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import './App.scss';
-import Home from './home/Home';
+//import Login from './pages/login/Login';
+//import Register from './pages/register/Register';
+import Home from './pages/home/Home';
+//import Watch from './pages/watch/Watch';
 
 function App() {
   return <Home />
--- client/src/components/listItem/ListItem.js
+++ client/src/components/listItem/ListItem.js
@@ -1,19 +1,44 @@
+import {useState} from 'react'
 import './ListItem.scss';
 import { PlayArrow, Add, ThumbUpAltOutlined, ThumbDownAltOutlined} from '@mui/icons-material'
 
-export default function ListItem() {
-    return (
-        <div className="listItem">
-            <img src="https://thumbnails.moviemania.io/desktop/movie/335984/74945c/1016x574.jpg" alt="" />
 
-            <div className="itemInfo">
-                <div className="icons">
-                    <PlayArrow />
-                    <Add />
-                    <ThumbUpAltOutlined />
-                    <ThumbDownAltOutlined />
-                </div>
-            </div>
+export default function ListItem({index}) {
+    const [isHovered, setIsHovered] = useState(false)
+    const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
+    return (
+        <div className="listItem" 
+        style={{left: isHovered && index * 225 - 50 + index * 2.5}}
+        onMouseEnter={() => setIsHovered(true)} 
+        onMouseLeave={() => setIsHovered(false)}>
+            <img src="https://thumbnails.moviemania.io/desktop/movie/335984/74945c/1016x574.jpg" alt="" />
+            
+            {isHovered && (
+                <>
+                    <video src={trailer} autoPlay={true} loop />
+
+                    <div className="itemInfo">
+                        <div className="icons">
+                            <PlayArrow className="icon" />
+                            <Add className="icon" />
+                            <ThumbUpAltOutlined className="icon" />
+                            <ThumbDownAltOutlined className="icon" />
+                        </div>
+
+                        <div className="itemInfoTop">
+                            <span>1 hour 14 mins</span>
+                            <span className="limit">+16</span>
+                            <span>2021</span>
+                        </div>
+
+                        <div className="desc">
+                            lorem ipsum dolor sit amet, consectet lorem ipsum d lorem ipsum dolor sit amet, consectet
+                        </div>
+
+                        <div className="genre">Action</div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
